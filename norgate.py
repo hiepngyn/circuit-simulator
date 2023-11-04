65fe0bb426c4dc6133cd39a02b4e7943f4f47c4c
jschaef <jwschaef@ksu.edu>
2023-11-03 21:27:41 -0500

    Hopefully resolved save conflic

--- norgate.py
+++ norgate.py
@@ -1,1 +1,19 @@
 # NOR Gate File
+class OR_gate:
+    top_Input = True
+    bottom_Input = True
+    output = True
+
+    def set_output(self):
+        self.output = not (self.top_Input or self.bottom_Input)
+    
+
+    def set_TopInput(self, input_Val):
+        self.top_Input = input_Val
+    
+    def set_BottomInput(self, input_Val):
+        self.bottom_Input = input_Val
+
+
+    def get_output(self):
+        return self.output
